c4c860801ba8420517b874ed314326a02bcf19c2
wiKapo <71190167+wiKapo@users.noreply.github.com>
2026-08-11 21:08:43 +0200

    Aktualizacja działania wybory loggera
    Dodanie nowego pliku `error.log`, do którego będą wysyłane wszystkie błędy

--- g/classes/logger.py
+++ g/classes/logger.py
@@ -4,10 +4,10 @@ from enum import Enum
 
 
 class LogType(Enum):  # when adding something that will need a new folder, add it to init_logger()
-    ALL = ""
+    DEFAULT = "default"
     CALENDAR = "calendar"
     USER = "user"
-    NOTIFICATION = "notification"
+    EVENT = "event"
 
 
 def init_logger():
@@ -15,18 +15,18 @@ def init_logger():
         os.makedirs('logs/calendar')
     if not os.path.exists('logs/user'):
         os.makedirs('logs/user')
+    if not os.path.exists('logs/event'):
+        os.makedirs('logs/event')
 
 
-def get_logger(log_type: LogType = LogType.ALL, id: int | None = None) -> logging.Logger:
+def get_logger(log_type: LogType = LogType.DEFAULT, data: str | None = None) -> logging.Logger:
     match log_type:
-        case LogType.CALENDAR | LogType.USER:
-            logger_name = f"{log_type.value}_{id if id else "default"}"
-        case LogType.NOTIFICATION:
-            logger_name = log_type.value
-        case LogType.ALL | _:
+        case LogType.CALENDAR | LogType.USER | LogType.EVENT:
+            logger_name = f"{log_type.value}_{data if data else "default"}"
+        case LogType.DEFAULT | _:
             logger_name = "default"
 
-    folder = "" if log_type in (LogType.ALL, LogType.NOTIFICATION) else f"{log_type.value}/"
+    folder = "" if log_type is LogType.DEFAULT else f"{log_type.value}/"
 
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.DEBUG)
@@ -36,5 +36,11 @@ def get_logger(log_type: LogType = LogType.ALL, id: int | None = None) -> loggin
         stream_handler.setStream(logging.FileHandler(f"logs/{folder}{logger_name}.log").stream)
         stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
         logger.addHandler(stream_handler)
+
+        error_handler = logging.StreamHandler()
+        error_handler.setStream(logging.FileHandler(f"logs/error.log").stream)
+        error_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        error_handler.addFilter(lambda record: record.levelno == logging.ERROR)
+        logger.addHandler(error_handler)
 
     return logger
